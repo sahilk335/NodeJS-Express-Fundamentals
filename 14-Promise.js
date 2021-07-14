44d69e643021c70ebae30d988687b22a8a08317a
Sahil Khurana <sahilk335@gmail.com>
2021-07-13 17:09:50 -0700

    Promise.js

---
+++ 14-Promise.js
@@ -0,0 +1,18 @@
+const { readFile } = require('fs');
+
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data);
+            }
+        });
+    })
+};
+
+getText('./content/first.txt')
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+
